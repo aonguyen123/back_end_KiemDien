277fecc225309faf91f370d3e4412d9d862bcf12
unknown <aonguyen113@gmail.com>
2019-11-10 10:46:00 +0700

    update

--- user/routes/presences.js
+++ user/routes/presences.js
@@ -6,7 +6,7 @@ const presencesController = require('./../controller/presences');
 Router.post('/createPresences', passport.authenticate('jwt', {session: false}), (req, res) => {
     actionPresencesController.createPresences(req, res);
 });
-Router.get('/getPresences/:id', (req, res) => {
+Router.get('/getPresences/:id', passport.authenticate('jwt', {session: false}), (req, res) => {
     presencesController.getPresences(req, res);
 });
 module.exports = Router;
